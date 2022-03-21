5d7451d367eccc2e2e1969edd10307e6f42ef25b
Scott Doyle <scott.doyle@mutualofomaha.com>
2022-03-21 16:44:54 -0500

    fix: fix format

--- release.config.js
+++ release.config.js
@@ -2,9 +2,9 @@ module.exports = {
     branches: "main",
     repositoryUrl: "https://github.com/smdoyle/react-app",
     plugins: [
-        "@semantic-release/commit-analyzer", 
-        "@semantic-release/release-notes-generator", 
-        "@semantic-release/npm", 
-        "@semantic-release/github"
-    ]
-}
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/github",
+    ],
+};
