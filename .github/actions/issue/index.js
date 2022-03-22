64329d6b508ecb0af6102ed6a21cb3a49717a8cb
Scott Doyle <scott.doyle@mutualofomaha.com>
2022-03-22 14:08:50 -0500

    feat(asdf): sdf
    fdsddfsdsfsdaf

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -2,24 +2,24 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  try {
-    const token = core.getInput("token");
-    const title = core.getInput("title");
-    const body = core.getInput("body");
-    const assignees = core.getInput("assignees");
+    try {
+        const token = core.getInput("token");
+        const title = core.getInput("title");
+        const body = core.getInput("body");
+        const assignees = core.getInput("assignees");
 
-    const octokit = new github.getOctokit(token);
-    const response = await octokit.rest.issues.create({
-       ...github.context.repo,
-       title,
-       body,
-       assignees: assignees ? assignees.split(",") : undefined,
-    });
+        const octokit = new github.getOctokit(token);
+        const response = await octokit.rest.issues.create({
+            ...github.context.repo,
+            title,
+            body,
+            assignees: assignees ? assignees.split(",") : undefined,
+        });
 
-    core.setOutput("issue", JSON.stringify(response.data));
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        core.setOutput("issue", JSON.stringify(response.data));
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
