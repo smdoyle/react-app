de9b9ac957bbc226bca7cac3c4e3101473d975d5
Scott Doyle <scott.doyle@mutualofomaha.com>
2022-03-22 14:24:23 -0500

    feat(asdf): asdf
    adsfdf

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -6,7 +6,7 @@ echo "::error ::Error Message"
 
 echo "::add-mask::$1"
 time=$(date)
-echo "::set-output name=time::"
+echo "::set-output name=time::$time"
 
 echo "::group::Some expandable logs"
 echo 'some stuff'
